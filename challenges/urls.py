5fb42630a1394061f81471014647bfbfa0d2d0e2
inward <inw@ukr.net>
2021-03-16 22:04:28 +0200

    all_about_url

---
+++ challenges/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("<int:month>", views.monthly_challenge_by_num),
+    path("<str:month>", views.monthly_challenge, name='str_path'),
+    path("", views.index),
+
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+monthly_challenges = {
+    'january':'todo challenge of january',
+    'february': 'todo challenge for february',
+    'march': 'todo challenge for march',
+    'april': 'todo challenge for april',
+    'may': 'todo challenge for may',
+}
+
+def index(request):
+    return HttpResponse("This works")
+
+def monthly_challenge(request, month):
+    return HttpResponse(monthly_challenges.get(month))
+
+
+def monthly_challenge_by_num(request, month):
+    string_month = list(monthly_challenges.keys())[month-1]
+    redirect_path = reverse('str_path', args=[string_month])
+    return HttpResponseRedirect(redirect_path)
+
+    
